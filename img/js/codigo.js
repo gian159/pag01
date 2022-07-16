a1bdbd4c70e6aa3cc9e37366b2a5c73211ac2ba8
gian159 <108946339+gian159@users.noreply.github.com>
2022-07-15 20:03:20 -0500

    Delete img directory

--- img/js/codigo.js
+++
@@ -1,7 +0,0 @@
-const iconoMenu = document.querySelector('#icono-menu'), menu = document.querySelector('#menu');
-
-iconoMenu.addEventListener('click', (e) => {
-    menu.classList.toggle('active');
-    document.body.classList.toggle('opacity');
-});
-
